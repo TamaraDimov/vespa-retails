ac42578c69f247de914b91ac1c12f4d2b9652d07
kifle23 <kiflekifle23@gmail.com>
2023-09-21 18:00:51 +0300

    Create reservation controller actions

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::ReservationsController < ApplicationController
+    # before_action :authenticate_user!
+    before_action :set_reservation, only: [:show, :destroy]
+
+    def index
+    #   reservations = current_user.reservations
+      @user = User.first
+      reservations = @user.reservations
+      render json: reservations
+    end
+  
+    def show
+      render json: @reservation
+    end
+  
+    def create
+      reservation = Reservation.new(reservation_params)
+  
+      if reservation.save
+        render json: reservation, status: :created
+      else
+        render json: reservation.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @reservation.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    end
+  
+    def reservation_params
+      params.require(:reservation).permit(:start_date, :end_date, :city, :user_id, :motorcycle_id)
+    end  
 end
