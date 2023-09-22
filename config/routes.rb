db8f92193154de755c0c1abf33322ec98d12aa0e
kifle23 <kiflekifle23@gmail.com>
2023-09-22 16:27:10 +0300

    Renew credential after merge and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
  namespace :api do
   namespace :v1 do
     resources :motorcycles
-    resources :reservations, except: [:new, :edit]s
+    resources :reservations, except: [:new, :edit]
   end
  end
 end
