f802610c806fda91564cf5cb9fead4041afe578a
kifle23 <kiflekifle23@gmail.com>
2023-09-22 07:23:11 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
  namespace :api do
   namespace :v1 do
-    resources :motorcycles do
-      resources :reservations, except: [:new, :edit]
-    end
+    resources :motorcycles
+    resources :reservations, except: [:new, :edit]
   end
  end
 end
