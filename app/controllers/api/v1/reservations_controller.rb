39b36b71bc930116deb5f38d3bf03c18360666e9
kifle23 <kiflekifle23@gmail.com>
2023-09-23 10:19:47 +0300

    Fix offences

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,38 +1,38 @@
 class Api::V1::ReservationsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_reservation, only: [:show, :destroy]
+  before_action :authenticate_user!
+  before_action :set_reservation, only: %i[show destroy]
 
-    def index
-      reservations = current_user.reservations.includes(:motorcycle)
-      render json: reservations.to_json(include: { motorcycle: { only: :name } })
+  def index
+    reservations = current_user.reservations.includes(:motorcycle)
+    render json: reservations.to_json(include: { motorcycle: { only: :name } })
+  end
+
+  def show
+    render json: @reservation
+  end
+
+  def create
+    reservation = Reservation.new(reservation_params)
+
+    if reservation.save
+      render json: reservation, status: :created
+    else
+      render json: reservation.errors, status: :unprocessable_entity
     end
-  
-    def show
-      render json: @reservation
-    end
-  
-    def create
-      reservation = Reservation.new(reservation_params)
-  
-      if reservation.save
-        render json: reservation, status: :created
-      else
-        render json: reservation.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @reservation.destroy
-      head :no_content
-    end
-  
-    private
-  
-    def set_reservation
-      @reservation = Reservation.find(params[:id])
-    end
-  
-    def reservation_params
-      params.require(:reservation).permit(:start_date, :end_date, :city, :user_id, :motorcycle_id)
-    end  
+  end
+
+  def destroy
+    @reservation.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :city, :user_id, :motorcycle_id)
+  end
 end
