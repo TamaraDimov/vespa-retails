4717bcaac57e89cc4ce97a02c49769c7f1da8f0f
Besufekad-HAZ <besurye@gmail.com>
2023-09-25 17:35:52 +0300

    Revert "Write all the tests for controllers, models and auth tokens for sample test data"
    This reverts commit b87dfd801992a89d269239d900c98a278d97f35b.

---
+++ spec/controllers/api/v1/reservations_controller_controller_spec.rb
@@ -0,0 +1,119 @@
+# # spec/controllers/api/v1/reservations_controller_spec.rb
+# require 'rails_helper'
+
+# RSpec.describe Api::V1::ReservationsController, type: :controller do
+#   let(:user) { create(:user) }
+#   let(:motorcycle) { create(:motorcycle, user: user) }
+#   let(:reservation) { create(:reservation, user: user, motorcycle: motorcycle) }
+
+#   describe 'GET #index' do
+#     it 'returns all reservations for the current user' do
+#       sign_in(user)
+#       get :index
+#       expect(response).to have_http_status(:success)
+#       expect(response.body).to include(reservation.motorcycle.name)
+#     end
+#   end
+
+#   describe 'GET #show' do
+#     it 'returns a reservation if found' do
+#       sign_in(user)
+#       get :show, params: { id: reservation.id }
+#       expect(response).to have_http_status(:success)
+#       expect(response.body['id']).to eq(reservation.id)
+#     end
+
+#     it 'returns not_found if the reservation is not found' do
+#       sign_in(user)
+#       get :show, params: { id: 999 }
+#       expect(response).to have_http_status(:not_found)
+#       expect(response.body).to include('Resource not found')
+#     end
+#   end
+
+#   describe 'POST #create' do
+#     let(:valid_params) { { start_date: Date.today, end_date: Date.tomorrow, city: 'Test City', user_id: user.id, motorcycle_id: motorcycle.id } }
+#     let(:invalid_params) { { start_date: '', end_date: '', cityI apologize for the incomplete response earlier. Here's the complete code for the `MotorcyclesController` and `ReservationsController` tests:
+
+# ```ruby
+# # spec/controllers/api/v1/motorcycles_controller_spec.rb
+# require 'rails_helper'
+
+# RSpec.describe Api::V1::MotorcyclesController, type: :controller do
+#   let(:user) { create(:user) }
+#   let(:motorcycle) { create(:motorcycle, user: user) }
+
+#   describe 'GET #index' do
+#     it 'returns all motorcycles for the current user' do
+#       sign_in(user)
+#       get :index
+#       expect(response).to have_http_status(:success)
+#       expect(response.body).to include(motorcycle.name)
+#     end
+
+#     it 'returns not_found if no motorcycles are found' do
+#       sign_in(user)
+#       user.motorcycles.destroy_all
+#       get :index
+#       expect(response).to have_http_status(:not_found)
+#       expect(response.body).to include('Resource not found')
+#     end
+#   end
+
+#   describe 'GET #show' do
+#     it 'returns a motorcycle if found' do
+#       sign_in(user)
+#       get :show, params: { id: motorcycle.id }
+#       expect(response).to have_http_status(:success)
+#       expect(response.body['id']).to eq(motorcycle.id)
+#     end
+
+#     it 'returns not_found if motorcycle is not found' do
+#       sign_in(user)
+#       get :show, params: { id: 999 }
+#       expect(response).to have_http_status(:not_found)
+#       expect(response.body).to include('Resource not found')
+#     end
+#   end
+
+#   describe 'POST #create' do
+#     let(:valid_params) { { name: 'Test Motorcycle', model: 'Model XYZ', description: 'Test Description', photo: 'photo_url' } }
+#     let(:invalid_params) { { name: '', model: '', description: '', photo: '' } }
+
+#     context 'with valid parameters' do
+#       it 'creates a new motorcycle' do
+#         sign_in(user)
+#         expect {
+#           post :create, params: valid_params
+#         }.to change(Motorcycle, :count).by(1)
+#         expect(response).to have_http_status(:created)
+#         expect(response.body).to include('Created')
+#       end
+#     end
+
+#     context 'with invalid parameters' do
+#       it 'returns unprocessable_entity' do
+#         sign_in(user)
+#         post :create, params: invalid_params
+#         expect(response).to have_http_status(:unprocessable_entity)
+#         expect(response.body).to include('The request parameters are invalid')
+#       end
+#     end
+#   end
+
+#   describe 'DELETE #destroy' do
+#     it 'deletes a motorcycle' do
+#       sign_in(user)
+#       delete :destroy, params: { id: motorcycle.id }
+#       expect(response).to have_http_status(:ok)
+#       expect(response.body).to include('Motorcycle deleted successfully')
+#     end
+
+#     it 'returns not_found if motorcycle is not found' do
+#       sign_in(user)
+#       delete :destroy, params: { id: 999 }
+#       expect(response).to have_http_status(:not_found)
+#       expect(response.body).to include('Motorcycle not found')
+#     end
+#   end
+# end
