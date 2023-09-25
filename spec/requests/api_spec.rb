2089b53b80696b23ac479f841e31b27f8127f306
Besufekad-HAZ <besurye@gmail.com>
2023-09-25 13:17:34 +0300

    Add more tests for create and delete motor cylces

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -33,3 +33,95 @@ RSpec.describe 'API', type: :request do
         run_test!
       end
     end
+    post 'Create a motorcycle' do
+      tags 'Motorcycles'
+      consumes 'application/json'
+      parameter name: :motorcycle, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          model: { type: :string },
+          description: { type: :string },
+          photo: { type: :string }
+        },
+        required: ['name', 'model', 'description', 'photo']
+      }
+
+      response '201', 'Created' do
+        run_test!
+      end
+
+      response '422', 'Unprocessable Entity' do
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/motorcycles/{id}' do
+    parameter name: :id, in: :path, type: :integer, required: true
+
+    get 'Retrieve a specific motorcycle' do
+      tags 'Motorcycles'
+      produces 'application/json'
+
+      response '200', 'OK' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 model: { type: :string },
+                 description: { type: :string },
+                 photo: { type: :string },
+                 user_id: { type: :integer }
+               },
+               required: ['id', 'name', 'model', 'description', 'photo', 'user_id']
+
+        run_test!
+      end
+
+      response '404', 'Resource not found' do
+        run_test!
+      end
+    end
+
+    delete 'Delete a motorcycle' do
+      tags 'Motorcycles'
+
+      response '200', 'OK' do
+        run_test!
+      end
+
+      response '404', 'Resource not found' do
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/reservations' do
+    get 'Retrieve all reservations' do
+      tags 'Reservations'
+      produces 'application/json'
+
+      response '200', 'OK' do
+        examples 'application/json' => [
+          {
+            id: 1,
+            start_date: '2023-09-25',
+            end_date: '2023-09-28',
+            city: 'City 1',
+            user_id: 1,
+            motorcycle_id: 1
+          },
+          {
+            id: 2,
+            start_date: '2023-10-01',
+            end_date: '2023-10-05',
+            city: 'City 2',
+            user_id: 1,
+            motorcycle_id: 2
+          }
+        ]
+
+        run_test!
+      end
+    end
