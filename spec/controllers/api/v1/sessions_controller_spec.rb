23ad3aa60e18b987f0bf8678c2fa11960560e5d5
Besufekad-HAZ <besurye@gmail.com>
2023-10-03 16:58:25 +0300

    Fix tests and delete tests folder as it's unnecessary

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -10,3 +10,11 @@
 #     end
 #   end
 # end
+
+require 'rails_helper'
+
+RSpec.describe 'Simple test', type: :test do
+  it 'can load its configuration and run an example' do
+    expect(true).to be_truthy
+  end
+end
--- spec/controllers/api/v1/simple_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Simple test', type: :test do
-  it 'can load its configuration and run an example' do
-    expect(true).to be_truthy
-  end
-end
